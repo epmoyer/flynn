7566c94710bcce65b42efbebc9634bd309f62c9a
Eric Moyer <eric@lemoncrab.com>
2017-01-01 20:19:57 -0800

    add event_simlulator

--- js/lib/flynn/flynnUtil.js
+++ js/lib/flynn/flynnUtil.js
@@ -189,6 +189,75 @@ Flynn.Util = {
         return(Math.pow(normalized, power)*(max-min) + min);
     },
 
+    event_simulator: {
+        // Issues a simulated event to a dom element.
+        // Adapted from http://stackoverflow.com/questions/6157929/how-to-simulate-a-mouse-click-using-javascript/6158050#6158050
+
+        simulate: function(element, eventName)
+        {
+            var sim = Flynn.Util.event_simulator;
+            var options = sim.extend(sim.defaultOptions, arguments[2] || {});
+            var oEvent, eventType = null;
+
+            for (var name in sim.eventMatchers)
+            {
+                if (sim.eventMatchers[name].test(eventName)) { eventType = name; break; }
+            }
+
+            if (!eventType)
+                throw new SyntaxError('Only HTMLEvents and MouseEvents interfaces are supported');
+
+            if (document.createEvent)
+            {
+                oEvent = document.createEvent(eventType);
+                if (eventType == 'HTMLEvents')
+                {
+                    oEvent.initEvent(eventName, options.bubbles, options.cancelable);
+                }
+                else
+                {
+                    oEvent.initMouseEvent(eventName, options.bubbles, options.cancelable, document.defaultView,
+                    options.button, options.pointerX, options.pointerY, options.pointerX, options.pointerY,
+                    options.ctrlKey, options.altKey, options.shiftKey, options.metaKey, options.button, element);
+                }
+                element.dispatchEvent(oEvent);
+            }
+            else
+            {
+                options.clientX = options.pointerX;
+                options.clientY = options.pointerY;
+                var evt = document.createEventObject();
+                oEvent = sim.extend(evt, options);
+                element.fireEvent('on' + eventName, oEvent);
+            }
+            return element;
+        },
+
+        extend: function(destination, source) {
+            for (var property in source)
+              destination[property] = source[property];
+            return destination;
+        },
+
+        eventMatchers: {
+            'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
+            'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
+        },
+
+        defaultOptions: {
+            pointerX: 0,
+            pointerY: 0,
+            button: 0,
+            ctrlKey: false,
+            altKey: false,
+            shiftKey: false,
+            metaKey: false,
+            bubbles: true,
+            cancelable: true
+        }
+
+    },
+
 
 };
 
