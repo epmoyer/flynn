f4b425e452ac356bdbf954b6123d1c14289a63d8
Eric Moyer <eric@lemoncrab.com>
2018-04-08 16:24:31 -0700

    use sphinx_rtd_theme

--- docs/conf.py
+++ docs/conf.py
@@ -17,8 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sphinx_rtd_theme
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -83,7 +84,11 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
+
+# Add any paths that contain custom themes here, relative to this directory.
+# html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
